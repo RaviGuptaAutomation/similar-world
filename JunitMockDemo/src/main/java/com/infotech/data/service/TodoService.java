a7b53e294092f518a55f1d6acd382dba69e4ce5e
Ravi <Ravi@02HW029546.NOAM.TCS.com>
2018-09-19 21:50:47 -0500

    commit

---
+++ JunitMockDemo/src/main/java/com/infotech/data/service/TodoService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.infotech.data.service;
+
+import java.util.List;
+
+/**
+ * @author Ravi
+ *
+ */
+public interface TodoService { 
+	
+	public List<String> retreiveTodos(String user);
+
+}
